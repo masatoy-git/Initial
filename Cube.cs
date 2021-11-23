2a5ce2ccea1445622388397722326ed856090ff8
masatoy <86176162+masatoy-git@users.noreply.github.com>
2021-11-23 21:22:49 +0900

    Add files via upload

---
+++ Cube.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UIの動きテストくん
+        /*
+        不必要
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameObject UICanvas = GameObject.FindGameObjectWithTag("UICanvas");
+            GameManager nanikore = UICanvas.GetComponent<GameManager>();
+            nanikore.AFKBarEffect(1);
+        }
+        if(Input.GetKeyDown(KeyCode.Return)){
+            GameObject UICanvas = GameObject.FindGameObjectWithTag("UICanvas");
+            GameManager nanikore = UICanvas.GetComponent<GameManager>();
+            nanikore.AFKBarEffect(2);
+        }*/
+    }
+}
